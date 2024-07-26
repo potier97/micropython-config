366588bb74457df1cd3d1bf6d85f6f212d2c1f21
potier97 <nicolaspotier97@gmail.com>
2024-07-26 00:32:09 -0500

    feature: remove comments

--- scripts/servo.py
+++ scripts/servo.py
@@ -3,15 +3,7 @@ import time
 from servo import Servo
 
 led = machine.Pin(16, machine.Pin.OUT)
-# Configura el pin PWM
-#servo_pin = machine.Pin(14, machine.Pin.OUT)  # Cambia el pin si es necesario
-#pwm = machine.PWM(servo_pin, freq=50)
 motor=Servo(pin=14)
-# Función para establecer el ángulo del servo
-#def set_servo_angle(angle):
-#    # Convierte el ángulo (0-180) a un ciclo de trabajo de PWM
-#    duty = int((angle / 180.0 * 102) + 26)
-#    pwm.duty(duty)
 
 while True:
     # Mover el servo a 0 grados
